b6346e0f45e2765cf9d3918cfef118225c0f2e6f
sebouh137 <sebouh.paul@gmail.com>
2022-08-17 10:53:45 -0700

    updated elevations in GEMC; updated parquet2lund converter

--- corsika/parquet2lund.py
+++ corsika/parquet2lund.py
@@ -14,13 +14,16 @@ elevation=5300
 inputfile=None
 outputfile=None
 
+theta=0
+phi=0
+
 def print_usage():
     print("""
     useage example:
 
-    python parquet2lund.py --elevation 5300 --input in.parquet --output out.lund
+    python parquet2lund.py --elevation 5450 --input in.parquet --output out.lund -z zenith -a azimuth
 
-    converts in.parquet to out.lund, printing out only particles that cross the z= 5300 m plane (approximate elevation of condor).  The "elevation" argument is optional.  
+    converts in.parquet to out.lund, extrapolating all particle's trajectories to z=5350 (the approximate elevation of CONDOR + 50 m).  The "elevation" argument is optional.  The zenith and azimuth are in degrees.  If omitted, they default to 0
     """)
 
     
@@ -34,6 +37,12 @@ for i in range(1,len(sys.argv)):
     if sys.argv[i] == "--output" and len(sys.argv)>=i:
         outputfile=sys.argv[i+1]
         continue
+    if sys.argv[i] in "--zenith -z".split() and len(sys.argv)>=i:
+        theta=np.pi/180*float(sys.argv[i+1])
+        continue
+    if sys.argv[i] in "--azimuth -a".split() and len(sys.argv)>=i:
+        phi=np.pi/180*float(sys.argv[i+1])
+        continue
 if inputfile==None or outputfile==None:
     print_usage()
     exit()
@@ -42,7 +51,7 @@ df = pq.read_pandas(inputfile).to_pandas()
 nevents = len(set(df.shower))
 UNUSED =0
 
-condition=f"end_z<{elevation} and start_z>{elevation}"
+#condition=f"end_z<{elevation} and start_z>{elevation}"
 #condition='pdg==130'
 
 masses={}
@@ -51,12 +60,12 @@ pdgdb = ROOT.TDatabasePDG()
 
 with open(outputfile, "w") as out:
     for shower in range(nevents):
-        subdf = df.query(f"shower == {shower} and {condition}")
+        subdf = df.query(f"shower == {shower}")
         print(len(subdf), UNUSED, UNUSED, UNUSED, UNUSED, UNUSED, UNUSED, UNUSED, UNUSED, UNUSED,file=out, sep='\t') 
         ii=0
         for i in subdf.index:
             ii+=1
-            norm = np.sqrt((subdf.end_x[i]-subdf.start_x[i])**2+(subdf.end_y[i]-subdf.start_y[i])**2+(subdf.end_z[i]-subdf.start_z[i])**2)
+            #norm = np.sqrt((subdf.end_x[i]-subdf.start_x[i])**2+(subdf.end_y[i]-subdf.start_y[i])**2+(subdf.end_z[i]-subdf.start_z[i])**2)
             pdg = int(subdf.pdg[i])
             if pdg in masses.keys():
                 m = masses[pdg]
@@ -71,15 +80,22 @@ with open(outputfile, "w") as out:
                 
             E  = subdf.energy[i]
             p = np.sqrt(E**2-m**2)
-            px = p*(subdf.end_x[i]-subdf.start_x[i])/norm
-            py = p*(subdf.end_y[i]-subdf.start_y[i])/norm
-            pz = p*(subdf.end_z[i]-subdf.start_z[i])/norm
+            px = p*np.cos(phi)*np.sin(theta)
+            py = p*np.sin(phi)*np.sin(theta)
+            pz = -p*np.cos(theta)
 
 
             
-            vx = subdf.start_x[i]*100
-            vy = subdf.start_y[i]*100
-            vz = subdf.start_z[i]*100
+            vx = subdf.x[i]*100
+            vy = subdf.y[i]*100
+            vz = subdf.z[i]*100
+
+            #extrapolate to the correct elevation
+            if elevation is not None:
+                vx -= np.tan(theta)*np.cos(phi)*(elevation-vz)
+                vy -= np.tan(theta)*np.sin(phi)*(elevation-vz)
+                vz = elevation
+                
             print(ii, UNUSED, 1, subdf.pdg[i], UNUSED,UNUSED, px,py, pz, E, m, vx,vy,vz, file=out, sep='\t')
             #print(subdf[i].shower)
         del subdf
--- gemc/mockup_elevations.py
+++ gemc/mockup_elevations.py
@@ -1,6 +1,26 @@
+import numpy as np
+
+#relative elevations of the 4 corners
+SW=0
+SE=23.9
+NW=17.0
+NE=32.5
+
+ncol=14
+nrow=14
+
+#slopes
+ai=(SE+NE-SW-NW)/(2*(ncol-1))
+aj=(NW+NE-SW-SE)/(2*(nrow-1))
+#quadratic term
+aij=(NE-NW+SW-SE)/((ncol-1)*(nrow-1))
+
+all_elevations=[]
 with open("elevations.txt","w") as f:
-    #print("#row,column,elevation",file=f)
-    for i in range(14):
-        for j in range(14):
-            elevation=(i-7)*6+(j-7)*10+(j-7)**2*0.3
+    #print("#column, row ,elevation",file=f)
+    for i in range(ncol):
+        for j in range(nrow):
+            elevation=(i-(ncol-1)/2)*ai+(j-(nrow-1)/2)*aj+(i-(ncol-1)/2)*(j-(nrow-1)/2)*aij
             print(i+1,j+1, elevation,sep=',',file=f)
+            all_elevations.append(elevation)
+print("max elevation", np.max(all_elevations))
